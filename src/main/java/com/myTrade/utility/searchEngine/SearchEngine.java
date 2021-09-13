80db36764671b6396405d9fff712d3a4e716de3d
bartbard <bartosz.mucha@outlook.com>
2021-09-13 23:20:27 +0200

    Sort engine has been fixed

--- src/main/java/com/myTrade/utility/searchEngine/SearchEngine.java
+++ src/main/java/com/myTrade/utility/searchEngine/SearchEngine.java
@@ -6,15 +6,16 @@ import lombok.Data;
 import java.util.List;
 
 import static com.myTrade.utility.searchEngine.FilterEngine.*;
+import static com.myTrade.utility.searchEngine.SortEngine.sort;
 
 @Data
 public class SearchEngine {
     public static List<AdEntity> search(AdSearchRequest request, List<AdEntity> adList) {
-        return filterByName(request.getSearchByName(), request.getSearchInDescription(),
+        return sort(request.getSortType(),filterByName(request.getSearchByName(), request.getSearchInDescription(),
                 filterByPrice(request.getPriceRange(),
                         filterByCity(request.getCity(),
                                 filterByCategory(request.getAdCategory(),
-                                        adList))));
+                                        adList)))));
     }
 
 
