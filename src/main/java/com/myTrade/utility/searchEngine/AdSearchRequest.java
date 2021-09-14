a46cfecf57717246db94ac5f2c8cd8315603d83f
bartbard <bartosz.mucha@outlook.com>
2021-09-14 23:34:57 +0200

    Optionals have been added to AdSearchRequest and FilterEngine

--- src/main/java/com/myTrade/utility/searchEngine/AdSearchRequest.java
+++ src/main/java/com/myTrade/utility/searchEngine/AdSearchRequest.java
@@ -3,15 +3,19 @@ package com.myTrade.utility.searchEngine;
 import com.myTrade.utility.AdCategory;
 import lombok.Data;
 
+import java.util.Optional;
+
 @Data
 public class AdSearchRequest {
 
     private String searchByName;
-    private AdCategory adCategory;
-    private PriceRange priceRange;
-    private Boolean searchInDescription;
-    private String city;
+    private Optional<AdCategory> adCategory;
+    private Optional<PriceRange> priceRange;
+    private Optional<Boolean> searchInDescription;
+    private Optional<String> city;
     private SortType sortType;
+    private Optional<Integer> pageNumber;
+    private Optional<Integer> pageSizeNumber;
 }
 
 
--- src/main/java/com/myTrade/utility/searchEngine/FilterEngine.java
+++ src/main/java/com/myTrade/utility/searchEngine/FilterEngine.java
@@ -6,11 +6,12 @@ import com.myTrade.utility.AdCategory;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class FilterEngine {
 
-    public static List<AdEntity> filterByName(String searchName, Boolean searchInDescription, List<AdEntity> adList) {
+    public static List<AdEntity> filterByName(String searchName, Optional<Boolean> searchInDescription, List<AdEntity> adList) {
         List<AdEntity> result = new LinkedList<>();
         if (!searchName.isEmpty()) {
             String[] providedWords = searchName.toLowerCase(Locale.ROOT).split(" ");
@@ -22,25 +23,25 @@ public class FilterEngine {
         return result.stream().distinct().collect(Collectors.toList());
     }
 
-    public static List<AdEntity> filterByPrice(PriceRange priceRange, List<AdEntity> adList) {
+    public static List<AdEntity> filterByPrice(Optional<PriceRange> priceRange, List<AdEntity> adList) {
         List<AdEntity> result = new LinkedList<>();
-        if (priceRange != null) {
-            result.addAll(adList.stream().filter(ad -> ad.getPrice() >= priceRange.getFrom() && ad.getPrice() <= priceRange.getTo()).collect(Collectors.toList()));
+        if (priceRange.isPresent()) {
+            result.addAll(adList.stream().filter(ad -> ad.getPrice() >= priceRange.get().getFrom() && ad.getPrice() <= priceRange.get().getTo()).collect(Collectors.toList()));
         } else result = adList;
         return result;
     }
 
-    public static List<AdEntity> filterByCategory(AdCategory adCategory, List<AdEntity> adList) {
+    public static List<AdEntity> filterByCategory(Optional<AdCategory> adCategory, List<AdEntity> adList) {
         List<AdEntity> result = new LinkedList<>();
-        if (adCategory != null) {
+        if (adCategory.isPresent()) {
             result.addAll(adList.stream().filter(ad -> ad.getAdCategory().equals(adCategory)).collect(Collectors.toList()));
         } else result = adList;
         return result;
     }
 
-    public static List<AdEntity> filterByCity(String city, List<AdEntity> adList) {
+    public static List<AdEntity> filterByCity(Optional<String> city, List<AdEntity> adList) {
         List<AdEntity> result = new LinkedList<>();
-        if (city != null) {
+        if (city.isPresent()) {
             result.addAll(adList.stream().filter(ad -> ad.getCity().equals(city)).collect(Collectors.toList()));
         } else result = adList;
         return result;
