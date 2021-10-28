37d65d87643fb5219875e1e382e30d6d5d154042
bartbard <bartosz.mucha@outlook.com>
2021-10-28 17:53:10 +0200

    Cors configuration updated

--- src/main/java/com/myTrade/security/CorsConfig.java
+++ src/main/java/com/myTrade/security/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-             registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+             registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+                     .allowedHeaders("Authorization")
+                     .allowedHeaders("Access-Control-Allow-Headers")
+                     .exposedHeaders("Authorization","Access-Control-Allow-Headers");
             }
         };
     }
