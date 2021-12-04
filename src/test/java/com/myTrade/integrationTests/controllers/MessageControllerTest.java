8e2306c7c5bf67fb162fe73be9e6d17866545b74
bartbard <bartosz.mucha@outlook.com>
2021-12-04 03:15:14 +0200

    New controllers integration tests

--- src/test/java/com/myTrade/integrationTests/controllers/MessageControllerTest.java
+++ src/test/java/com/myTrade/integrationTests/controllers/MessageControllerTest.java
@@ -2,41 +2,96 @@ package com.myTrade.integrationTests.controllers;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.myTrade.entities.MessageEntity;
+import com.myTrade.dto.MessageDto;
+import com.myTrade.repositories.ConversationRepository;
 import com.myTrade.repositories.MessageRepository;
+import com.myTrade.utility.UserUtility;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
+@WithMockUser(username = "brad@brad.brad")
 public class MessageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private MessageRepository messageRepository;
 
-    ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private ConversationRepository conversationRepository;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @ParameterizedTest
+    @MethodSource("messageDtoWithConversationId")
+    public void whenProperMessageDtoAndConversationIdIsProvided_thenMessageDtoShouldBeSavedAndRetrieved201(MessageDto messageDto, Long conversationId) {
+        //given
+        int ONE_ADDITIONAL_MESSAGE = 1;
+        int expectedMessageListSize = messageRepository.findMessageEntityListByConversationId(conversationId).size() + ONE_ADDITIONAL_MESSAGE;
+        //when & then
+        try {
+            mockMvc.perform(post("/message/save/{id}", String.valueOf(conversationId)).contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(messageDto)))
+                    .andDo(print())
+                    .andExpect(status().is(201));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        int actualMessageListSize = messageRepository.findMessageEntityListByConversationId(conversationId).size();
+        assertThat(actualMessageListSize).isEqualTo(expectedMessageListSize);
+    }
+
+    public static Stream<Arguments> messageDtoWithConversationId() {
+        return Stream.of(
+                Arguments.of(MessageDto.builder().text("Hi mike!").build(), 1L),
+                Arguments.of(MessageDto.builder().text("Nice!").build(), 1L),
+                Arguments.of(MessageDto.builder().text("I'm interested").build(), 1L),
+                Arguments.of(MessageDto.builder().text("Test").build(), 1L)
+        );
+    }
 
     @Test
-    public void whenProperMessageDtoIsProvided_thenRetrieved201() throws Exception {
+    public void whenConversationIdIsProvided_thenMessageDtoListShouldBeFetchedByConversationIdAndRetrieved200() {
         //given
-        MessageEntity message = new MessageEntity();
-        message.setAuthorId(1L);
-        message.setText("Lorem ipsum");
-        //when
-        //then
-        mockMvc.perform(post("/message/save").contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(message))).andExpect(status().is(201));
+        String username = UserUtility.getUsernameFromContext();
+        Long conversationId = conversationRepository
+                .findConversationEntityPageByRecipientOrSenderUsername(username, Pageable.unpaged())
+                .get()
+                .findFirst()
+                .get()
+                .getId();
+        //when & then
+        try {
+            mockMvc.perform(get("/message/fetch/list/{id}", String.valueOf(conversationId)))
+                    .andDo(print())
+                    .andExpect(status().is(200))
+                    .andExpect((content().contentType(MediaType.APPLICATION_JSON)))
+                    .andExpect(jsonPath("$").isArray())
+                    .andExpect(jsonPath("$").isNotEmpty());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
