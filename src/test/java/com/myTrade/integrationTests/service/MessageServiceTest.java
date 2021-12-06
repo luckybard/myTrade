cbdeffe51d68ec3efae907819dfc8dae83a00d15
bartbard <bartosz.mucha@outlook.com>
2021-12-06 06:26:28 +0200

    Services integration tests updated

--- src/test/java/com/myTrade/integrationTests/service/MessageServiceTest.java
+++ src/test/java/com/myTrade/integrationTests/service/MessageServiceTest.java
@@ -2,38 +2,68 @@ package com.myTrade.integrationTests.service;
 
 
 import com.myTrade.dto.MessageDto;
-import com.myTrade.entities.MessageEntity;
-import com.myTrade.repositories.MessageRepository;
+import com.myTrade.repositories.ConversationRepository;
 import com.myTrade.services.MessageService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.myTrade.utility.TestUtility.ONE_ADDITIONAL_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
+@WithMockUser(username = "brad@brad.brad")
 public class MessageServiceTest {
 
-    private MessageRepository messageRepository;
+
+    private ConversationRepository conversationRepository;
     private MessageService messageService;
 
     @Autowired
-    public MessageServiceTest(MessageRepository messageRepository, MessageService messageService) {
-        this.messageRepository = messageRepository;
+    public MessageServiceTest( ConversationRepository conversationRepository, MessageService messageService) {
+        this.conversationRepository = conversationRepository;
         this.messageService = messageService;
     }
 
-    @Test
-    public void whenMessageDtoIsProvided_shouldSetDateTimeAndBeSaved() {
+    @ParameterizedTest
+    @MethodSource("messageDtoWithConversationId")
+    public void whenValidMessageDtoAndConversationIdIsProvided_shouldSaveMessageAndAssignToConversation(MessageDto messageDto, Long conversationId){
         //given
-        MessageDto messageDto = new MessageDto();
-        messageDto.setAuthorId(1L);
-        messageDto.setText("Lorem ipsum");
+        int expectedConversationMessageAmount = conversationRepository.findById(conversationId).get().getMessageList().size() + ONE_ADDITIONAL_MESSAGE;
         //when
-        messageService.saveMessage(messageDto); //TODO: [Q] Is it possible to check saved message id?
+        messageService.saveMessageDtoAndAssignToConversationById(messageDto, conversationId);
+        int actualConversationMessageAmount = conversationRepository.findById(conversationId).get().getMessageList().size();
         //then
-        MessageEntity actual = messageRepository.findAll().stream().filter(messageEntity -> messageEntity.getAuthorId().equals(1L)&&messageEntity.getText().equalsIgnoreCase("Lorem ipsum")).findFirst().get();
-        assertThat(actual.getDateTime()).isNotNull();
+        assertThat(expectedConversationMessageAmount).isEqualTo(actualConversationMessageAmount);
+    }
 
+    private static Stream<Arguments> messageDtoWithConversationId() {
+        return Stream.of(
+                Arguments.of(MessageDto.builder().text("Hi Adam!").build(),1L),
+                Arguments.of(MessageDto.builder().text("Nice!").build(),1L),
+                Arguments.of(MessageDto.builder().text("I'm interested").build(),1L)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {1,2})
+    public void whenConversationIdIsProvided_thenMessageDtoListShouldBeFetchedByConversationId(long conversationId){
+        //given
+        int FIRST_MESSAGE = 0;
+        //when
+        List<MessageDto> listResponseEntity = messageService.fetchMessageDtoListByConversationId(conversationId);
+        //then
+        assertThat(listResponseEntity).isNotNull();
+        assertThat(listResponseEntity.get(FIRST_MESSAGE)).isNotNull();
+        assertThat(listResponseEntity.get(FIRST_MESSAGE).getText()).isNotEmpty();
     }
 }
