838ff01e85d3f1d989b453f02cc8dbb5a1f1d0b5
bartbard <bartosz.mucha@outlook.com>
2021-12-14 15:56:34 +0100

    Auth components styled

--- src/frontend/src/components/auth/signIn/SignInForm.js
+++ src/frontend/src/components/auth/signIn/SignInForm.js
@@ -1,46 +1,66 @@
 import React from "react";
 import useSignInForm from "../../../hooks/useSignInForm";
 import validate from "./ValidateSignInInfo";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const SignInForm = () => {
-    const {handleChange, handleSubmit, values, errors} = useSignInForm(
-        validate
-    );
+  const { handleChange, handleSubmit, values, errors } =
+    useSignInForm(validate);
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit} noValidate>
-                <h1>Sign in!</h1>
-                <div>
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="Enter your username"
-                        value={values.username}
-                        onChange={handleChange}
-                    />
-                    {errors.username && <p>{errors.username}</p>}
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Enter your password"
-                        value={values.password}
-                        onChange={handleChange}
-                    />
-                    {errors.password && <p>{errors.password}</p>}
-                </div>
-                <button type="submit">Sign in!</button>
-                <span>
-                    Don't have an account? Sign up! <Link to="/sign-up"><a href="/sign-up">here</a></Link>
-                </span>
-            </form>
-        </div>
-    );
+  return (
+    <Container>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+          marginTop: "7rem",
+        }}
+      >
+        <form onSubmit={handleSubmit} noValidate>
+          <h1>Sign in!</h1>
+          <div class="form-group">
+            <div>
+              <label htmlFor="username">Username</label>
+              <input
+                class="form-control"
+                type="text"
+                name="username"
+                placeholder="Enter your username"
+                value={values.username}
+                onChange={handleChange}
+              />
+              {errors.username && <p>{errors.username}</p>}
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                class="form-control"
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                value={values.password}
+                onChange={handleChange}
+              />
+              {errors.password && <p>{errors.password}</p>}
+            </div>
+            <div style={{ marginTop: "0.5rem" }}>
+              <button className="btn btn-primary" type="submit">
+                Sign in!
+              </button>
+              <span>
+                Don't have an account? Sign up!{" "}
+                <Link to="/sign-up">
+                  <a href="/sign-up">here</a>
+                </Link>
+              </span>
+            </div>
+          </div>
+        </form>
+      </div>
+    </Container>
+  );
 };
 
 export default SignInForm;
--- src/frontend/src/components/auth/signUp/SignUpForm.js
+++ src/frontend/src/components/auth/signUp/SignUpForm.js
@@ -1,71 +1,88 @@
 import React from "react";
 import useSignUpForm from "../../../hooks/useSignUpForm";
 import validate from "./ValidateSignUpInfo";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const SignUpForm = () => {
-  const { handleChange, handleSubmit, values, errors } = useSignUpForm(
-    validate
-  );
-  
+  const { handleChange, handleSubmit, values, errors } =
+    useSignUpForm(validate);
+
   return (
-    <div>
-      <form onSubmit={handleSubmit} noValidate>
-        <h1>
-          Get started now! Create your account by filling out the
-          information below.
-        </h1>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            placeholder="Enter your username"
-            value={values.username}
-            onChange={handleChange}
-          />
-          {errors.username && <p>{errors.username}</p>}
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter your email"
-            value={values.email}
-            onChange={handleChange}
-          />
-          {errors.email && <p>{errors.email}</p>}
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={values.password}
-            onChange={handleChange}
-          />
-          {errors.password && <p>{errors.password}</p>}
-        </div>
-        <div>
-          <label htmlFor="password2">Confirm password</label>
-          <input
-            type="password"
-            name="password2"
-            placeholder="Enter your password again"
-            value={values.password2}
-            onChange={handleChange}
-          />
-          {errors.password2 && <p>{errors.password2}</p>}
-        </div>
-        <button type="submit">Sign up!</button>
-        <span>
-          Already have an account? Login
-          <Link to="/sign-in"><a href="/sign-in">here</a></Link>
-        </span>
-      </form>
-    </div>
+    <Container>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+          marginTop: "7rem",
+        }}
+      >
+        <form onSubmit={handleSubmit} noValidate>
+          <h1>Get started now! Sign up!</h1>
+          <h5>Create your account by filling out the information below.</h5>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              class="form-control"
+              type="text"
+              name="username"
+              placeholder="Enter your username"
+              value={values.username}
+              onChange={handleChange}
+            />
+            {errors.username && <p>{errors.username}</p>}
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              class="form-control"
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              value={values.email}
+              onChange={handleChange}
+            />
+            {errors.email && <p>{errors.email}</p>}
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              class="form-control"
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              value={values.password}
+              onChange={handleChange}
+            />
+            {errors.password && <p>{errors.password}</p>}
+          </div>
+          <div>
+            <label htmlFor="password2">Confirm password</label>
+            <input
+              class="form-control"
+              type="password"
+              name="password2"
+              placeholder="Enter your password again"
+              value={values.password2}
+              onChange={handleChange}
+            />
+            {errors.password2 && <p>{errors.password2}</p>}
+          </div>
+          <div style={{ marginTop: "0.5rem" }}>
+            <button className="btn btn-primary" type="submit">
+              Sign up!
+            </button>
+            <span>
+              Already have an account? Login
+              <Link to="/sign-in">
+                <a href="/sign-in">here</a>
+              </Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </Container>
   );
 };
 
