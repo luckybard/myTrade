b6b8e9ef75cb8cf80bdd80db06b254d2ef0b6677
bartbard <bartosz.mucha@outlook.com>
2021-12-18 15:19:33 +0100

    Layout components added

---
+++ src/frontend/src/components/Layout/Header.js
@@ -0,0 +1,46 @@
+import {Fragment, useContext} from "react";
+import {NavLink} from "react-router-dom";
+import AuthContext from "../../store/auth-context";
+
+import classes from "./Header.module.css";
+
+const Header = () => {
+    const authCtx = useContext(AuthContext);
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1 className={classes.logo}>MyTrade</h1>
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <NavLink activeClassName={classes.active} to="/home">
+                                Home
+                            </NavLink>
+                        </li>
+                        {authCtx.isLoggedIn &&
+                        <li>
+                            <NavLink activeClassName={classes.active} to="/profile">
+                                Profile
+                            </NavLink>
+                        </li>
+                        }
+                        {!authCtx.isLoggedIn &&
+                        <li>
+                            <NavLink activeClassName={classes.active} to="/auth">
+                                Login
+                            </NavLink>
+                        </li>
+                        }
+                        <li>
+                            <NavLink activeClassName={classes.active} to="/create-ad">
+                                Create Ad
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </Fragment>
+    );
+};
+
+export default Header;
