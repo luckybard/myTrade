ed0b44e1835648920d277343e4c3fdf3a8f573df
bartbard <bartosz.mucha@outlook.com>
2021-12-18 15:22:52 +0100

    Index.js added

---
+++ src/frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AuthContextProvider } from "./store/auth-context";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
