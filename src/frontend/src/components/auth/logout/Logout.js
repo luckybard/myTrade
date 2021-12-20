1e3ab1bc3dfe31df9bca25f28111b39754337b64
bartbard <bartosz.mucha@outlook.com>
2021-12-20 17:26:02 +0100

    Logout page refactored

---
+++ src/frontend/src/components/auth/logout/Logout.js
@@ -0,0 +1,18 @@
+import React, {useContext, useEffect, useState} from "react";
+import {Redirect} from "react-router";
+import AuthContext from "../store/auth-context";
+
+const Logout = () => {
+    const authCtx = useContext(AuthContext)
+    const [isLogout, setIsLogout] = useState(false);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/logout").then(
+            setIsLogout(true));
+        authCtx.logout();
+    }, []);
+
+    return <div>{isLogout && <Redirect to="/sign-in"/>};</div>;
+};
+
+export default Logout;
