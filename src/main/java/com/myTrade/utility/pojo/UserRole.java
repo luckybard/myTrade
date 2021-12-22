c8d6603dc857c62666e4aaaf4678e9a8f0dc9320
bartbard <bartosz.mucha@outlook.com>
2021-12-22 03:10:53 +0100

    Typo fixed in UserRole

--- src/main/java/com/myTrade/utility/pojo/UserRole.java
+++ src/main/java/com/myTrade/utility/pojo/UserRole.java
@@ -1,14 +1,12 @@
 package com.myTrade.utility.pojo;
 
-import com.google.common.collect.Sets;
-
 import java.util.Set;
 
 import static com.myTrade.utility.pojo.UserPermission.*;
 
 public enum UserRole {
-    USER(Sets.newHashSet(AD_READ, AD_WRITE, CONVERSATION_READ, MESSAGE_WRITE)),
-    ADMIN(Sets.newHashSet(USER_WRITE, USER_READ, AD_READ, AD_WRITE));
+    USER(Set.of(AD_READ, AD_WRITE, CONVERSATION_READ, MESSAGE_WRITE)),
+    ADMIN(Set.of(USER_WRITE, USER_READ, AD_READ, AD_WRITE));
 
     private final Set<UserPermission> userPermissionsSet;
 
