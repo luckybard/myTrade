badc46ee3f51b79ae7bc09fd49aaf9c5e8a7eb06
bartbard <bartosz.mucha@outlook.com>
2021-12-23 19:50:28 +0100

    Profile page changed

--- src/frontend/src/pages/ProfilePage.js
+++ src/frontend/src/pages/ProfilePage.js
@@ -1,11 +1,20 @@
 import {Link} from "react-router-dom";
-import Card from "../components/UI/Card";
-
+import React from "react";
 
 const ProfilePage = () => {
     return (<div>
-        <Link to="/profile/userAds">User Ads </Link>
-        <Link to="/profile/userFavouriteAds">User Favourite Ads</Link>)
+        <div className="position-absolute top-50 start-50 translate-middle btn-group-vertical">
+            <Link style={{color: 'black'}}
+                  class="btn btn-lg btn-outline-warning" to="/profile/inbox">Inbox</Link>
+            <Link style={{color: 'black'}}
+                  class="btn btn-lg btn-outline-warning" to="/profile/userAds">Your Ads</Link>
+            <Link style={{color: 'black'}}
+                  class="btn btn-lg btn-outline-warning" to="/profile/userFavouriteAds">Your Favourite
+                Ads</Link>
+            <button type="button" className="btn btn-lg btn-secondary" disabled/>
+            <Link style={{color: 'black'}}
+                  class="btn btn-lg btn-outline-danger" to="/logout">Logout</Link>
+        </div>
     </div>)
 
 }
