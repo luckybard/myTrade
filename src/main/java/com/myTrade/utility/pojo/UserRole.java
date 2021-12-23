c607165a5eecf040666571c9bf24377e861c152a
bartbard <bartosz.mucha@outlook.com>
2021-12-24 00:38:24 +0100

    Additional authorities added to Role_User

--- src/main/java/com/myTrade/utility/pojo/UserRole.java
+++ src/main/java/com/myTrade/utility/pojo/UserRole.java
@@ -5,7 +5,7 @@ import java.util.Set;
 import static com.myTrade.utility.pojo.UserPermission.*;
 
 public enum UserRole {
-    USER(Set.of(AD_READ, AD_WRITE, CONVERSATION_READ, MESSAGE_WRITE)),
+    USER(Set.of(AD_READ, AD_WRITE, CONVERSATION_READ, MESSAGE_WRITE,USER_WRITE, USER_READ)),
     ADMIN(Set.of(USER_WRITE, USER_READ, AD_READ, AD_WRITE));
 
     private final Set<UserPermission> userPermissionsSet;
