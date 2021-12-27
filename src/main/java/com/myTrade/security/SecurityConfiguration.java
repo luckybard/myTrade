36b35a2f3404b0e2d89b77479b406419ccea876e
bartbard <bartosz.mucha@outlook.com>
2021-12-27 06:15:40 +0100

    Cors removed

--- src/main/java/com/myTrade/security/SecurityConfiguration.java
+++ src/main/java/com/myTrade/security/SecurityConfiguration.java
@@ -40,7 +40,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and()
                 .httpBasic()
                 .and()
                 .csrf().disable()
